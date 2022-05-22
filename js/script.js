c0c3afa77f47403beca3c9499c02be6e3b680684
jhwmac <jkr2505@naver.com>
2022-05-22 14:13:16 +0900

    산청군청 모달

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 window.onload = function () {
+    // 모달창
+    let $modal = $('.modal');
+    let $modal_close = $('.modal-close');
+    let $comment_bt = $('.comment-bt');
+    $modal_close.click(function () {
+        $('html').css('overflow-y', 'auto');
+        $modal.fadeOut(300);
+    });
+    $comment_bt.click(function () {
 
+        $(this).hide();
+        $(this).text('닫기').show(300);
+        if ($modal.hasClass('comment-on')) {
+            $(this).hide();
+            $(this).text('리뷰 및 개선사항').show(300);
+        }
+        $modal.toggleClass('comment-on');
+    });
     // aos 관련
     AOS.init({
         once: true
